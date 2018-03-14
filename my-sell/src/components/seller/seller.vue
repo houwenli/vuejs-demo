c75c13ac58c1f748d34d406849b5dc556864b54e
houwenli <1219027111@qq.com>
2018-03-15 07:46:56 +0800

    alt seller 18-3-15 0746

--- my-sell/src/components/seller/seller.vue
+++ my-sell/src/components/seller/seller.vue
@@ -1,7 +1,105 @@
 <template>
-	<div>
-		seller
-	</div>
+	<div class="seller">
+		<div class="seller-content">
+			<div class="overview" style="pointer-events: auto;">
+				<h1 class="title">粥品香坊（回龙观）</h1> 
+				<div class="desc border-1px">
+					<div class="star star-36">
+						<span class="star-item on"></span>
+						<span class="star-item on"></span>
+						<span class="star-item on"></span>
+						<span class="star-item on"></span>
+						<span class="star-item off"></span>
+					</div> 
+					<span class="text">(24)</span> 
+					<span class="text">月售90单</span>
+				</div>
+				<ul class="remark">
+					<li class="block">
+						<h2>起送价</h2> 
+						<div class="content">
+							<span class="stress">20</span>元
+	          			</div>
+	          		</li> 
+	          		<li class="block">
+	          			<h2>商家配送</h2> 
+	          			<div class="content">
+	          				<span class="stress">4</span>元
+	          			</div>
+	          		</li> 
+	          		<li class="block">
+	          			<h2>平均配送时间</h2> 
+	          			<div class="content">
+	          				<span class="stress">38</span>分钟
+	          			</div>
+	          		</li>
+	          	</ul> 
+	          	<div class="favorite">
+	          		<span class="icon-favorite"></span> 
+	          		<span class="text">收藏</span>
+	          	</div>
+	        </div> 
+        	<div class="split" style="pointer-events: auto;"></div> 
+        	<div class="bulletin" style="pointer-events: auto;">
+        		<h1 class="title">公告与活动</h1> 
+        		<div class="content-wrapper border-1px">
+        			<p class="content">粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。</p>
+        		</div> 
+        		<ul class="supports">
+        			<li class="support-item border-1px">
+        				<span class="icon decrease"></span> 
+        				<span class="text">在线支付满28减5</span>
+        			</li>
+        			<li class="support-item border-1px">
+        				<span class="icon discount"></span> 
+        				<span class="text">VC无限橙果汁全场8折</span>
+        			</li>
+        			<li class="support-item border-1px">
+        				<span class="icon special"></span> 
+        				<span class="text">单人精彩套餐</span>
+        			</li>
+        			<li class="support-item border-1px">
+        				<span class="icon invoice"></span> 
+        				<span class="text">该商家支持发票,请下单写好发票抬头</span>
+        			</li>
+        			<li class="support-item border-1px">
+        				<span class="icon guarantee"></span> 
+        				<span class="text">已加入“外卖保”计划,食品安全保障</span>
+        			</li>
+        		</ul>
+        	</div> 
+        	<div class="split" style="pointer-events: auto;"></div> 
+        	<div class="pics" style="pointer-events: auto;">
+        		<h1 class="title">商家实景</h1> 
+        		<div class="pic-wrapper">
+        			<ul class="pic-list">
+        				<li class="pic-item" style="pointer-events: auto;">
+        					<img src="http://fuss10.elemecdn.com/8/71/c5cf5715740998d5040dda6e66abfjpeg.jpeg?imageView2/1/w/180/h/180" width="120" height="90">
+        				</li>
+        				<li class="pic-item" style="pointer-events: auto;">
+        					<img src="http://fuss10.elemecdn.com/b/6c/75bd250e5ba69868f3b1178afbda3jpeg.jpeg?imageView2/1/w/180/h/180" width="120" height="90">
+        				</li>
+        				<li class="pic-item" style="pointer-events: auto;">
+        					<img src="http://fuss10.elemecdn.com/f/96/3d608c5811bc2d902fc9ab9a5baa7jpeg.jpeg?imageView2/1/w/180/h/180" width="120" height="90">
+        				</li>
+        				<li class="pic-item" style="pointer-events: auto;">
+        					<img src="http://fuss10.elemecdn.com/6/ad/779f8620ff49f701cd4c58f6448b6jpeg.jpeg?imageView2/1/w/180/h/180" width="120" height="90">
+        				</li>
+        			</ul>
+        		</div>
+        	</div> 
+        	<div class="split" style="pointer-events: auto;"></div> 
+        	<div class="info" style="pointer-events: auto;">
+        		<h1 class="title border-1px">商家信息</h1> 
+        		<ul>
+        			<li class="info-item">该商家支持发票,请下单写好发票抬头</li>
+        			<li class="info-item">品类:其他菜系,包子粥店</li>
+        			<li class="info-item">北京市昌平区回龙观西大街龙观置业大厦底商B座102单元1340</li>
+        			<li class="info-item">营业时间:10:00-20:30</li>
+        		</ul>
+        	</div>
+        </div>
+    </div>
 </template>
 
 <script>
